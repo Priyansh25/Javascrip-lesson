6fec67af7d758b72d1b7adb4eb2b93142dd149db
Priyansh25 <priyansh.dby25@gmail.com>
2021-08-13 17:59:16 +0530

    added more content

--- 6.functions/1.regular-function.js
+++ 6.functions/1.regular-function.js
@@ -11,3 +11,23 @@ console.log(name)
 // console.warn('Please check this')
 // console.info('Provide more info')
 // console.error('There is error')
+
+function getFullname (firstname, lastname){
+
+    return firstname + ' ' + lastname
+
+}
+var fullname = getFullname()
+console.log(fullname,typeof fullname)
+
+// var a = 2+1+"uma"+3+4
+// console.log(a,typeof a)
+
+// var a = undefined+"Hello"
+// console.log(a,typeof a)
+
+var fullname2 = getFullname("Adam")
+console.log(fullname2,typeof fullname2)
+
+var fullname3 = getFullname("Adam","colom")
+console.log(fullname3,typeof fullname3)
